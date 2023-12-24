3cd84d86f7d0d9a87ebe87b07fe208b3ed62b9ef
AnaRenchen <anamagbh@gmail.com>
2023-12-24 11:37:08 -0300

    save

--- prueba/prueba/src/assets/componentes/Logo.jsx
+++ prueba/prueba/src/assets/componentes/Logo.jsx
@@ -1,7 +1,7 @@
 import LogoHeader from "../imagenes/logo_header.jpg";
 
-const Logo = () => {
-  return <img className="image-fluid" width={80} src={LogoHeader} alt="" />;
+const Logo = (size) => {
+  return <img className="image-fluid" width={size} src={LogoHeader} alt="" />;
 };
 
 export default Logo;
